47f56e01b6f942ec565ab155529a6db75ac8e4b1
LegendsDatabase <deus1984@gmail.com>
2026-04-08 12:02:59 +0200

    fix column alignment for macros

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -4,3 +4,8 @@ export const supabase = createClient(
   import.meta.env.PUBLIC_SUPABASE_URL,
   import.meta.env.PUBLIC_SUPABASE_ANON_KEY
 );
+
+// 👇 AGGIUNGI QUESTO
+if (typeof window !== 'undefined') {
+  window.supabase = supabase;
+}
